6948852186864b30e49fcd197f2f2fd901fcc493
Cesar Andres Pereira Bernal <programacion@idealidad.co>
2023-11-24 17:54:22 -0500

    primer hola mundo con springBoot

---
+++ src/main/java/com/safases/market/SafasesMarketApplication.java
@@ -0,0 +1,13 @@
+package com.safases.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafasesMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafasesMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safases/market/SafasesMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safases.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafasesMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
